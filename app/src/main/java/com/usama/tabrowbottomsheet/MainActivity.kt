70bd4f532fa671f05547a35a81e297380e2f5440
Usama Bin Hamza <96182703+usama-bin-hamza@users.noreply.github.com>
2024-01-31 15:52:58 +0500

    Bottom sheet implemented

--- app/src/main/java/com/usama/tabrowbottomsheet/MainActivity.kt
+++ app/src/main/java/com/usama/tabrowbottomsheet/MainActivity.kt
@@ -3,15 +3,32 @@ package com.usama.tabrowbottomsheet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.usama.tabrowbottomsheet.ui.theme.TabRowBottomSheetTheme
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +39,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val scaffoldState = rememberBottomSheetScaffoldState()
+                    var isSheetOpen by rememberSaveable {
+                        mutableStateOf(false)
+                    }
+
+                    val scope = rememberCoroutineScope()
+
+                    BottomSheetScaffold(
+                        scaffoldState = scaffoldState,
+                        sheetContent = {
+                            Image(
+                                painter = painterResource(id = R.drawable.image),
+                                contentDescription = "image",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                            )
+                        },
+                        sheetPeekHeight = 0.dp
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            contentAlignment = Alignment.Center
+
+                        ) {
+                            Button(onClick = {
+                                scope.launch {
+                                    scaffoldState.bottomSheetState.expand()
+                                    isSheetOpen = true
+                                }
+                            }) {
+                                Text(text = "Open Sheet")
+                            }
+                        }
+                    }
+
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TabRowBottomSheetTheme {
-        Greeting("Android")
     }
 }
